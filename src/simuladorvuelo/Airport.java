c6233138a879a9d8b182574ae53281abad97e2d0
daolmedo <da.olmedon@gmail.com>
2015-02-20 12:40:45 +0000

    Version 1.0.0

---
+++ src/simuladorvuelo/Airport.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.zcasd
+ */
+package simuladorvuelo;
+
+/**
+ *
+ * @author dani
+ */
+public class Airport extends AirportObjects
+{
+    public Airport(float x, float y, float z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    
+    @Override
+    public void render()
+    {
+        System.out.println("Dibujo Aeropuerto en coord: " + x +", "+ y +", "+ z);
+    }
+
+}
