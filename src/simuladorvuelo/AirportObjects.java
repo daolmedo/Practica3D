b95c50c2d034feb0dc511bbf16a52056dd00345b
daolmedo <da.olmedon@gmail.com>
2015-02-28 20:09:58 +0100

    Version 1.0.2

---
+++ src/simuladorvuelo/AirportObjects.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simuladorvuelo;
+
+/**
+ *
+ * @author dani
+*/
+public abstract class AirportObjects 
+{
+    abstract void render();
+    
+    protected float x;
+    protected float y;
+    protected float z;
+    
+}
